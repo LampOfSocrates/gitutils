cb42872a94ec319481fb1a598383e2e963f6a877
LampOfSocrates <LampOfSocrates@gmail.com>
2019-06-29 13:13:23 +0100

    mychnages

--- README.md
+++ README.md
@@ -1,1 +1,2 @@
-"First Commit" 
+
+Keep these newrepo.bat and uploadrepo.bat in your windows\system32 folder ( since its already on ur path)
--- newrepo.bat
+++ newrepo.bat
@@ -1,4 +1,7 @@
 
+echo =================
+echo "Usage : newrepo <name of local and remote repo folder > < github account name>
+echo =================
 REM eg junk3
 mkdir %1
 
@@ -14,6 +17,6 @@ git commit -m "First Commit"
 
 REM eg https://github.com/LampOfSocrates/junk3.git
 
-git remote add origin %2
+git remote add origin https://github.com/%2/%1
 
 git push -u origin master
