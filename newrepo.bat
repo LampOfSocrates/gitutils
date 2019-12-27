a5f86ded9cdad4766fc0feb76eb551325e37ff39
LampOfSocrates <LampOfSocrates@gmail.com>
2019-12-27 08:44:31 +0000

    newrepo.bat

--- newrepo.bat
+++ newrepo.bat
@@ -1,22 +1,26 @@
+REM @echo off
 
-echo =================
-echo "Usage : newrepo <name of local and remote repo folder > < github account name>
-echo =================
-REM eg junk3
+for /f "tokens=*" %%a in ('git config user.name') do set GitUserName=%%a
+echo Using Git Account %GitUserName%
+if [%1]==[] goto usage
+
+
+REM First create the folder and move to it
 mkdir %1
-
 cd %1
 
+REM Init the local git repo and commit
 git init 
-
 echo "First Commit" >> README.md
-
 git add README.md
-
 git commit -m "First Commit"
 
-REM eg https://github.com/LampOfSocrates/junk3.git
 
-git remote add origin https://github.com/%2/%1
-
+REM Now add and push the remote git eg https://github.com/LampOfSocrates/junk3.git
+git remote add origin https://github.com/%GitUserName%/%1
 git push -u origin master
+
+
+:usage
+@echo Usage: %0 ^<name of local and remote repo folder^> 
+exit /B 1
