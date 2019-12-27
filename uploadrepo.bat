8953b0ecf38fada41b94faeddd9bb2dd73369b6e
LampOfSocrates <LampOfSocrates@gmail.com>
2019-12-27 08:49:23 +0000

    general improvments

--- uploadrepo.bat
+++ uploadrepo.bat
@@ -1,4 +1,11 @@
+REM @echo off
+
+if [%1]==[] goto usage
 git add -A 
 git commit -m %1
 REM git remote add origin %2
 git push -u origin master
+
+:usage
+@echo Usage: %0 ^"Some helpful comments for today^" 
+exit /B 1
